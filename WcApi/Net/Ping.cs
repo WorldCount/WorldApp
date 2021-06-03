9cd0fe1109f305d13abe84e530595186b70f2338
WorldCount <World.Count@yandex.ru>
2021-06-03 19:30:26 +0300

    Добавлен класс определения доступности хоста

---
+++ WcApi/Net/Ping.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace WcApi.Net
+{
+    public static class Ping
+    {
+        public static bool GetPing(string host)
+        {
+            Process cmd = new Process
+            {
+                StartInfo =
+                {
+                    StandardOutputEncoding = Encoding.GetEncoding("cp866"),
+                    FileName = "cmd.exe",
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true,
+                    UseShellExecute = false
+                }
+            };
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine($"ping -n 1 -w 1 {host}");
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+
+            string result = cmd.StandardOutput.ReadToEnd();
+            return result.ToUpper().Contains("TTL");
+        }
+    }
+}
