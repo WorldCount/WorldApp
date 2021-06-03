6a282d27eb7d8f80c29f62a63763ec3b8e00ac9b
WorldCount <World.Count@yandex.ru>
2021-06-03 19:28:59 +0300

    Добавлен класс получения IP адреса хоста

---
+++ WcApi/Net/Host.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WcApi.Net
+{
+    public static class Host
+    {
+        public static string GetIp()
+        {
+            string host = Dns.GetHostName();
+            List<IPAddress> ipAddresses = Dns.GetHostAddresses(host).Where(a => a.AddressFamily == AddressFamily.InterNetwork).ToList();
+            return ipAddresses.Count > 0 ? ipAddresses[0].ToString() : "localhost";
+        }
+    }
+}
