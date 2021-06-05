208671f8095905d7d5bac3db6e1d2aa1c340b5a8
WorldCount <World.Count@yandex.ru>
2021-06-05 22:23:12 +0300

    Добавлен класс для работы с DBF

---
+++ WcApi/Database/DBFReader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data;
+using System.Data.Odbc;
+using System.Windows.Forms;
+
+namespace WcApi.Database
+{
+    // ReSharper disable once InconsistentNaming
+    public class DBFReader
+    {
+        private readonly OdbcConnection _connection;
+
+        public DBFReader(string path)
+        {
+            _connection = new OdbcConnection
+            {
+                ConnectionString = @"Driver={Microsoft dBase Driver (*.dbf)};" + $"DBQ={path};" + @"SourceType=DBF;Exclusive=No;Collate=Machine;NULL=NO;DELETED=NO;BACKGROUNDFETCH=NO;"
+            };
+        }
+
+        public DataTable Execute(string command)
+        {
+            DataTable dt = null;
+            if (_connection != null)
+            {
+                try
+                {
+                    _connection.Open();
+                    dt = new DataTable();
+                    OdbcCommand cmd = _connection.CreateCommand();
+                    cmd.CommandText = command;
+                    dt.Load(cmd.ExecuteReader());
+                    _connection.Close();
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message, "DBF: Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            return dt;
+        }
+
+        public DataTable GetAll(string dbPath)
+        {
+            return Execute($"select * from {dbPath}");
+        }
+    }
+}
