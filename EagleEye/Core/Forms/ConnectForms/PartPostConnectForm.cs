9b960ffb6065d7087f75f5c149754ca9c0b76b1d
WorldCount <World.Count@yandex.ru>
2021-06-05 22:05:54 +0300

    Макет формы подключение к Партионной почте

--- EagleEye/Core/Forms/ConnectForms/PartPostConnectForm.cs
+++ EagleEye/Core/Forms/ConnectForms/PartPostConnectForm.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,80 @@ namespace EagleEye.Core.Forms.ConnectForms
         public PartPostConnectForm()
         {
             InitializeComponent();
+
+            // ReSharper disable once VirtualMemberCallInConstructor
+            Text = $@"{Properties.Settings.Default.AppName}: Подключение";
+
+
+            // TODO: Доделать инициализацию
+
+        }
+
+        private void PartPostConnectForm_Load(object sender, EventArgs e)
+        {
+            // TODO: Биндинг к форме
+        }
+
+        private void tbStatus_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void tbPort_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void ConnectForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Нажатие Ctrl + S
+            if (e.KeyCode == Keys.S && e.Control)
+                btnSave.PerformClick();
+
+            // Esc
+            if (e.KeyCode == Keys.Escape)
+                btnCancel.PerformClick();
+        }
+
+        private async void btnCheck_Click(object sender, EventArgs e)
+        {
+            // TODO: Проверка подключения к партионной почте
+        }
+
+        private void btnAuto_Click(object sender, EventArgs e)
+        {
+            string host = Dns.GetHostName();
+            // ИП адресса
+            List<IPAddress> ipAddresses = Dns.GetHostAddresses(host).Where(a => a.AddressFamily == AddressFamily.InterNetwork).ToList();
+            tbHost.Text = ipAddresses.Count > 0 ? ipAddresses[0].ToString() : @"localhost";
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog { Filter = @"PARTPOST.IB|PARTPOST.IB" };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (openFileDialog.FileName != "")
+                {
+                    tbPath.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // TODO: Сделать сохранение - SaveConnect();
+            DialogResult = DialogResult.OK;
+            Close();
         }
     }
 }
