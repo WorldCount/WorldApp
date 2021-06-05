2071a9a7a7e6532486ae7a5459068b9fc5e65abd
WorldCount <World.Count@yandex.ru>
2021-06-05 22:25:30 +0300

    Добавленны классы расширения для списков и текста

---
+++ WcApi/Ext/TextExt.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace WcApi.Ext
+{
+    public static class TextExt
+    {
+        // ReSharper disable once InconsistentNaming
+        public static string RemoveIntLINQ(this string input)
+        {
+            return new string(input.Where(char.IsLetter).ToArray());
+        }
+
+        public static string RemoveIntRegex(this string input)
+        {
+            return Regex.Replace(input, @"[\d]", string.Empty);
+        }
+    }
+}
