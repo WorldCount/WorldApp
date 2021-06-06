de35bc080e496b08b39e9f8bd8d808003096a319
WorldCount <World.Count@yandex.ru>
2021-06-06 21:47:32 +0300

    Добавленна возможность изменения бордера для кнопки

--- Wc32Api/Widgets/WcButton.cs
+++ Wc32Api/Widgets/WcButton.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing;
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
@@ -6,20 +8,66 @@ namespace Wc32Api.Widgets
 {
     public class WcButton : Button
     {
-        GraphicsPath GetRoundPath(RectangleF Rect, int radius)
+        private int _borderRadius = 4;
+        private float _borderThickness = 1;
+        private Color _borderColor = Color.Silver;
+
+        [Category("Appearance")]
+        [Description("Defines the radius of the controls RoundedCorners.")]
+        public int BorderRadius
+        {
+            get => _borderRadius;
+            set
+            {
+                if (_borderRadius == value)
+                    return;
+                _borderRadius = value;
+                Invalidate();
+            }
+        }
+
+        [Category("Appearance")]
+        [Description("Defines the radius of the controls RoundedCorners.")]
+        public float BorderThickness
+        {
+            get => _borderThickness;
+            set
+            {
+                if (_borderThickness == value)
+                    return;
+                _borderThickness = value;
+                Invalidate();
+            }
+        }
+
+        [Category("Appearance")]
+        [Description("Defines the radius of the controls RoundedCorners.")]
+        public Color BorderColor
+        {
+            get => _borderColor;
+            set
+            {
+                if (_borderColor == value)
+                    return;
+                _borderColor = value;
+                Invalidate();
+            }
+        }
+
+        GraphicsPath GetRoundPath(RectangleF rect, int radius)
         {
             float m = 2.75F;
             float r2 = radius / 2f;
             GraphicsPath graphPath = new GraphicsPath();
 
-            graphPath.AddArc(Rect.X + m, Rect.Y + m, radius, radius, 180, 90);
-            graphPath.AddLine(Rect.X + r2 + m, Rect.Y + m, Rect.Width - r2 - m, Rect.Y + m);
-            graphPath.AddArc(Rect.X + Rect.Width - radius - m, Rect.Y + m, radius, radius, 270, 90);
-            graphPath.AddLine(Rect.Width - m, Rect.Y + r2, Rect.Width - m, Rect.Height - r2 - m);
-            graphPath.AddArc(Rect.X + Rect.Width - radius - m, Rect.Y + Rect.Height - radius - m, radius, radius, 0, 90);
-            graphPath.AddLine(Rect.Width - r2 - m, Rect.Height - m, Rect.X + r2 - m, Rect.Height - m);
-            graphPath.AddArc(Rect.X + m, Rect.Y + Rect.Height - radius - m, radius, radius, 90, 90);
-            graphPath.AddLine(Rect.X + m, Rect.Height - r2 - m, Rect.X + m, Rect.Y + r2 + m);
+            graphPath.AddArc(rect.X + m, rect.Y + m, radius, radius, 180, 90);
+            graphPath.AddLine(rect.X + r2 + m, rect.Y + m, rect.Width - r2 - m, rect.Y + m);
+            graphPath.AddArc(rect.X + rect.Width - radius - m, rect.Y + m, radius, radius, 270, 90);
+            graphPath.AddLine(rect.Width - m, rect.Y + r2, rect.Width - m, rect.Height - r2 - m);
+            graphPath.AddArc(rect.X + rect.Width - radius - m, rect.Y + rect.Height - radius - m, radius, radius, 0, 90);
+            graphPath.AddLine(rect.Width - r2 - m, rect.Height - m, rect.X + r2 - m, rect.Height - m);
+            graphPath.AddArc(rect.X + m, rect.Y + rect.Height - radius - m, radius, radius, 90, 90);
+            graphPath.AddLine(rect.X + m, rect.Height - r2 - m, rect.X + m, rect.Y + r2 + m);
 
             graphPath.CloseFigure();
             return graphPath;
@@ -27,14 +75,12 @@ namespace Wc32Api.Widgets
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            int borderRadius = 4;
-            float borderThickness = 1;
             base.OnPaint(e);
             RectangleF Rect = new RectangleF(0, 0, Width, Height);
-            GraphicsPath graphPath = GetRoundPath(Rect, borderRadius);
+            GraphicsPath graphPath = GetRoundPath(Rect, _borderRadius);
 
             Region = new Region(graphPath);
-            using (Pen pen = new Pen(Color.Silver, borderThickness))
+            using (Pen pen = new Pen(_borderColor, _borderThickness))
             {
                 pen.Alignment = PenAlignment.Inset;
                 e.Graphics.DrawPath(pen, graphPath);
