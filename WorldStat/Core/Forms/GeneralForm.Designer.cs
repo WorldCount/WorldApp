d1c668729716dbd5767bc71ab3aa00bb130c20db
WorldCount <World.Count@yandex.ru>
2021-07-31 10:08:25 +0300

    Инициализация приложения WorldStat

---
+++ WorldStat/Core/Forms/GeneralForm.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace WorldStat.Core.Forms
+{
+    partial class GeneralForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // GeneralForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(747, 440);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "GeneralForm";
+            this.Text = "WorldStat";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ WorldStat/Core/Forms/GeneralForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace WorldStat.Core.Forms
+{
+    public partial class GeneralForm : Form
+    {
+        public GeneralForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
