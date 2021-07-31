1b7432fe0051ec1c8963e763b8c63167dcb52934
WorldCount <World.Count@yandex.ru>
2021-08-01 00:21:25 +0300

    Виджет прогрессбара

---
+++ Wc32Api/Widgets/WcProgressBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Wc32Api.Widgets
+{
+    public class WcProgressBar : ProgressBar
+    {
+        public WcProgressBar()
+        {
+            SetStyle(ControlStyles.UserPaint, true);
+            DrawingControl.SetDoubleBuffered(this);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Rectangle rec = e.ClipRectangle;
+
+            rec.Width = (int)(rec.Width * ((double)Value / Maximum)) - 4;
+
+            if (ProgressBarRenderer.IsSupported)
+                ProgressBarRenderer.DrawHorizontalBar(e.Graphics, e.ClipRectangle);
+
+            rec.Height = rec.Height - 4;
+
+            e.Graphics.FillRectangle(new SolidBrush(BackColor), 2, 2, rec.Width, rec.Height);
+        }
+    }
+}
