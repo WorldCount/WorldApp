e60c5ece6247747b9cfd8f87ed98ed111789745c
WorldCount <World.Count@yandex.ru>
2021-08-21 12:25:51 +0300

    Отлов ошибок на уровне всего приложения

--- WorldStat/Program.cs
+++ WorldStat/Program.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 using System.Windows.Forms;
 using WorldStat.Core.Forms;
+using NLog;
 
 namespace WorldStat
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
+
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.ThreadException += Application_ThreadException;
             Application.Run(new GeneralForm());
+        }
+
+        private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            Logger.Error($"{e.Exception.Source}: {e.Exception}");
         }
     }
 }
