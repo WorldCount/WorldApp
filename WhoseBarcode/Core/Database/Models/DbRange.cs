8cbf7d3e610f933f6d3f90b9d8cc82a2cfbd1f91
WorldCount <World.Count@yandex.ru>
2021-08-22 13:54:53 +0300

    Добавление и правка моделей

--- WhoseBarcode/Core/Database/Models/DbRange.cs
+++ WhoseBarcode/Core/Database/Models/DbRange.cs
@@ -2,25 +2,22 @@
 
 namespace WhoseBarcode.Core.Database.Models
 {
-    public class DbBarcode
+    public class DbRange
     {
+        public int Id { get; set; }
         public DateTime Date { get; set; }
-
-        public string Barcode { get; set; }
+        public bool IsExternal { get; set; }
 
         public string Ops { get; set; }
         public int Month { get; set; }
         public int Seria { get; set; }
-        public string Num { get; set; }
-
-        public int RangeId { get; set; }
         
+
         public string FirmName { get; set; }
         public string FirmInn { get; set; }
         public string FirmDepcode { get; set; }
         public string FirmKpp { get; set; }
 
-        public int StateId { get; set; }
-        public string State { get; set; }
+        public int Count { get; set; }
     }
 }
