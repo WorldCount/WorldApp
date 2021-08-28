2c4b60ff2ee6d2949220dba4e7c47bc54f160530
WorldCount <World.Count@yandex.ru>
2021-08-28 13:49:29 +0300

    Инициализация DiffPather

---
+++ DiffPather/Core/Storage/PathManager.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Windows.Forms;
+
+namespace DiffPather.Core.Storage
+{
+    public static class PathManager
+    {
+        // Папки
+        public static readonly string DataDir = Path.Combine(Application.StartupPath, Properties.Settings.Default.DataDir);
+
+
+        static PathManager()
+        {
+            if (!File.Exists(DataDir))
+                Directory.CreateDirectory(DataDir);
+        }
+    }
+}
---
+++ DiffPather/Program.cs
@@ -0,0 +1,27 @@
+﻿using DiffPather.Core.Forms;
+using NLog;
+using System;
+using System.Windows.Forms;
+
+namespace DiffPather
+{
+    static class Program
+    {
+
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.ThreadException += Application_ThreadException;
+            Application.Run(new GeneralForm());
+        }
+
+        private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            Logger.Error($"{e.Exception.Source}: {e.Exception}");
+        }
+    }
+}
