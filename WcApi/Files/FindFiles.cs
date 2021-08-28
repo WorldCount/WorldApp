eff6ba89d65aa48184009e16f51f9baa88cd7461
WorldCount <World.Count@yandex.ru>
2021-08-28 18:44:13 +0300

    Поиск файлов в директории рекурсивно

---
+++ WcApi/Files/FindFiles.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace WcApi.Files
+{
+    public static class FindFiles
+    {
+        public static string[] SearchAllFiles(string directory)
+        {
+            List<string> files = new List<string>();
+
+            try
+            {
+                foreach (string r in Directory.GetFiles(directory))
+                    files.Add(r);
+
+                foreach (string d in Directory.GetDirectories(directory))
+                {
+                    foreach (string f in Directory.GetFiles(d))
+                        files.Add(f);
+                }
+
+                return files.ToArray();
+            }
+            catch
+            {
+                return files.ToArray();
+            }
+        }
+    }
+}
