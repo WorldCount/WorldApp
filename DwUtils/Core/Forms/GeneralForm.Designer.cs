d9c2ef279ccafc4ea728a0f4f6044ffa998fcefa
WorldCount <World.Count@yandex.ru>
2021-09-02 17:48:31 +0300

    DwUtils инициализация

---
+++ DwUtils/Core/Forms/GeneralForm.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace DwUtils.Core.Forms
+{
+    partial class GeneralForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuBar = new System.Windows.Forms.MenuStrip();
+            this.fileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFrankReportMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importCalendarMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createDbMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infosMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelGeneral = new System.Windows.Forms.Panel();
+            this.statusBar = new System.Windows.Forms.StatusStrip();
+            this.statusText = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusAuthor = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timerStatus = new System.Windows.Forms.Timer(this.components);
+            this.menuBar.SuspendLayout();
+            this.statusBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuBar
+            // 
+            this.menuBar.AutoSize = false;
+            this.menuBar.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.menuBar.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.menuBar.GripMargin = new System.Windows.Forms.Padding(0);
+            this.menuBar.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenuItem,
+            this.settingsMenuItem,
+            this.infosMenuItem});
+            this.menuBar.Location = new System.Drawing.Point(0, 0);
+            this.menuBar.Name = "menuBar";
+            this.menuBar.Padding = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuBar.Size = new System.Drawing.Size(800, 40);
+            this.menuBar.TabIndex = 3;
+            // 
+            // fileMenuItem
+            // 
+            this.fileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadFrankReportMenuItem,
+            this.importCalendarMenuItem,
+            this.exitMenuItem});
+            this.fileMenuItem.Name = "fileMenuItem";
+            this.fileMenuItem.Size = new System.Drawing.Size(57, 36);
+            this.fileMenuItem.Text = "Файл";
+            // 
+            // loadFrankReportMenuItem
+            // 
+            this.loadFrankReportMenuItem.Name = "loadFrankReportMenuItem";
+            this.loadFrankReportMenuItem.Size = new System.Drawing.Size(263, 24);
+            this.loadFrankReportMenuItem.Text = "Загрузить отчет по франку";
+            // 
+            // importCalendarMenuItem
+            // 
+            this.importCalendarMenuItem.Name = "importCalendarMenuItem";
+            this.importCalendarMenuItem.Size = new System.Drawing.Size(263, 24);
+            this.importCalendarMenuItem.Text = "Импорт календаря";
+            // 
+            // exitMenuItem
+            // 
+            this.exitMenuItem.Name = "exitMenuItem";
+            this.exitMenuItem.Size = new System.Drawing.Size(263, 24);
+            this.exitMenuItem.Text = "Выход";
+            // 
+            // settingsMenuItem
+            // 
+            this.settingsMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createDbMenuItem});
+            this.settingsMenuItem.Name = "settingsMenuItem";
+            this.settingsMenuItem.Size = new System.Drawing.Size(96, 36);
+            this.settingsMenuItem.Text = "Настройки";
+            // 
+            // createDbMenuItem
+            // 
+            this.createDbMenuItem.Name = "createDbMenuItem";
+            this.createDbMenuItem.Size = new System.Drawing.Size(205, 24);
+            this.createDbMenuItem.Text = "Создать новую БД";
+            // 
+            // infosMenuItem
+            // 
+            this.infosMenuItem.Name = "infosMenuItem";
+            this.infosMenuItem.Size = new System.Drawing.Size(60, 36);
+            this.infosMenuItem.Text = "Инфо";
+            // 
+            // panelGeneral
+            // 
+            this.panelGeneral.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(56)))), ((int)(((byte)(58)))));
+            this.panelGeneral.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelGeneral.ForeColor = System.Drawing.Color.Gainsboro;
+            this.panelGeneral.Location = new System.Drawing.Point(0, 40);
+            this.panelGeneral.Name = "panelGeneral";
+            this.panelGeneral.Size = new System.Drawing.Size(800, 58);
+            this.panelGeneral.TabIndex = 5;
+            // 
+            // statusBar
+            // 
+            this.statusBar.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.statusBar.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.statusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusText,
+            this.statusAuthor});
+            this.statusBar.Location = new System.Drawing.Point(0, 428);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(800, 22);
+            this.statusBar.TabIndex = 6;
+            this.statusBar.Text = "statusStrip1";
+            // 
+            // statusText
+            // 
+            this.statusText.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.statusText.Name = "statusText";
+            this.statusText.Size = new System.Drawing.Size(671, 17);
+            this.statusText.Spring = true;
+            // 
+            // statusAuthor
+            // 
+            this.statusAuthor.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.statusAuthor.ForeColor = System.Drawing.Color.Gray;
+            this.statusAuthor.Name = "statusAuthor";
+            this.statusAuthor.Size = new System.Drawing.Size(114, 17);
+            this.statusAuthor.Text = "WorldCount, 2021 ©";
+            // 
+            // timerStatus
+            // 
+            this.timerStatus.Interval = 3000;
+            this.timerStatus.Tick += new System.EventHandler(this.timerStatus_Tick);
+            // 
+            // GeneralForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusBar);
+            this.Controls.Add(this.panelGeneral);
+            this.Controls.Add(this.menuBar);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(56)))), ((int)(((byte)(58)))));
+            this.KeyPreview = true;
+            this.Name = "GeneralForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "GeneralForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GeneralForm_FormClosing);
+            this.Load += new System.EventHandler(this.GeneralForm_Load);
+            this.menuBar.ResumeLayout(false);
+            this.menuBar.PerformLayout();
+            this.statusBar.ResumeLayout(false);
+            this.statusBar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuBar;
+        private System.Windows.Forms.ToolStripMenuItem fileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFrankReportMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importCalendarMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createDbMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infosMenuItem;
+        private System.Windows.Forms.Panel panelGeneral;
+        private System.Windows.Forms.StatusStrip statusBar;
+        private System.Windows.Forms.ToolStripStatusLabel statusText;
+        private System.Windows.Forms.ToolStripStatusLabel statusAuthor;
+        private System.Windows.Forms.Timer timerStatus;
+    }
+}
+
---
+++ DwUtils/Core/Forms/GeneralForm.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DwUtils.Core.Forms
+{
+    public partial class GeneralForm : Form
+    {
+        public GeneralForm()
+        {
+            InitializeComponent();
+
+            // Если было обновление приложения
+            if (Properties.Settings.Default.NeedUpgrade)
+                UpgradeSettings();
+
+            // ReSharper disable once VirtualMemberCallInConstructor
+            // ReSharper disable once LocalizableElement
+            Text = $"{Properties.Settings.Default.AppName} {Application.ProductVersion}";
+
+            // Загрузка настроек
+            LoadSettings();
+        }
+
+        #region Form Config
+
+        // Загрузка настроек
+        private void LoadSettings()
+        {
+
+        }
+
+        // Сохранение настроек
+        private void SaveSettings()
+        {
+
+        }
+
+        // Перенос настроек предыдущей сборки в новую
+        private void UpgradeSettings()
+        {
+            Properties.Settings.Default.Upgrade();
+            Properties.Settings.Default.NeedUpgrade = false;
+            Properties.Settings.Default.Save();
+        }
+
+        // Загрузка расположения окна
+        private void LoadPos()
+        {
+            int width = Settings.FormsPosition.Default.GeneralFormSize.Width;
+            int height = Settings.FormsPosition.Default.GeneralFormSize.Height;
+
+            if (width == 0 || height == 0)
+            {
+                // Первый старт
+                StartPosition = FormStartPosition.CenterScreen;
+            }
+            else
+            {
+                WindowState = Settings.FormsPosition.Default.GeneralFormState;
+
+                // Нам не нужно свернутое окно при запуске
+                if (WindowState == FormWindowState.Minimized)
+                    WindowState = FormWindowState.Normal;
+
+                Location = Settings.FormsPosition.Default.GeneralFormLocation;
+                Size = Settings.FormsPosition.Default.GeneralFormSize;
+            }
+        }
+
+        // Сохранение расположения окна
+        private void SavePos()
+        {
+            Settings.FormsPosition.Default.GeneralFormState = WindowState;
+
+            if (WindowState == FormWindowState.Normal)
+            {
+                Settings.FormsPosition.Default.GeneralFormLocation = Location;
+                Settings.FormsPosition.Default.GeneralFormSize = Size;
+            }
+            else
+            {
+                // Если форма свернута или развернута
+                Settings.FormsPosition.Default.GeneralFormLocation = RestoreBounds.Location;
+                Settings.FormsPosition.Default.GeneralFormSize = RestoreBounds.Size;
+            }
+
+            Settings.FormsPosition.Default.Save();
+        }
+
+        // Чтение переданных параметров
+        private void CheckArgs()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+
+            // Восстановление положения окна
+            if (args.Contains("-restore"))
+                CenterToScreen();
+        }
+
+        private async void GeneralForm_Load(object sender, EventArgs e)
+        {
+
+            LoadPos();
+
+            // Чтение аргументов
+            CheckArgs();
+        }
+
+        private void GeneralForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveSettings();
+            SavePos();
+        }
+
+        #endregion
+
+        #region Messages
+
+        /// <summary>
+        /// Отправляет сообщение в статус
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="color"></param>
+        private void SendStatucMessage(string message, Color color)
+        {
+            statusText.ForeColor = color;
+            statusText.Text = message;
+            timerStatus.Start();
+        }
+
+        // Сообщение: обычное
+        // ReSharper disable once UnusedMember.Global
+        public void NormalMessage(string message)
+        {
+            SendStatucMessage(message, Color.DimGray);
+        }
+
+        // Сообщение: успех
+        public void SuccessMessage(string message)
+        {
+            SendStatucMessage(message, Color.Green);
+        }
+
+        // Сообщение: ошибка
+        public void ErrorMessage(string message)
+        {
+            SendStatucMessage(message, Color.Firebrick);
+        }
+
+        // Сообщение: предупреждение
+        public void WarningMessage(string message)
+        {
+            SendStatucMessage(message, Color.DarkOrange);
+        }
+
+        // Сообщение: информация
+        public void InfoMessage(string message)
+        {
+            SendStatucMessage(message, Color.DodgerBlue);
+        }
+
+        private void timerStatus_Tick(object sender, EventArgs e)
+        {
+            statusText.Text = "";
+        }
+
+        #endregion
+
+    }
+}
