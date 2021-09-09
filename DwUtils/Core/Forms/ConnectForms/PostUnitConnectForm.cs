890b1acac90960e30c7820d8c754f8407f3139ff
WorldCount <World.Count@yandex.ru>
2021-09-09 13:56:52 +0300

    Убран дизайн

--- DwUtils/Core/Forms/ConnectForms/PostUnitConnectForm.cs
+++ DwUtils/Core/Forms/ConnectForms/PostUnitConnectForm.cs
@@ -4,7 +4,6 @@ using System.Drawing;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using DwUtils.Core.Database.Connects;
 
@@ -15,7 +14,6 @@ namespace DwUtils.Core.Forms.ConnectForms
 
         #region Private Fields
 
-        private const int BorderSize = 2;
         private readonly PostUnitConnect _connect;
 
         #endregion
@@ -30,175 +28,8 @@ namespace DwUtils.Core.Forms.ConnectForms
             // ReSharper disable once VirtualMemberCallInConstructor
             Text = $"{Properties.Settings.Default.AppName}: Подключение к PostUnit";
 
-            Padding = new Padding(BorderSize);
-            // Border Color
-            BackColor = Color.FromArgb(45,45,48);
-
             WcApi.Keyboard.Keyboard.SetEnglishLanguage();
         }
-
-        #region Design
-
-        #region Overriddes Properties
-
-        public override string Text
-        {
-            get => base.Text;
-            set
-            {
-                base.Text = value;
-                labelTitle.Text = value;
-            }
-        }
-
-        #endregion
-
-        #region Extension
-
-        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
-        private static extern void ReleaseCapture();
-
-        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
-        private static extern void SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
-
-        #endregion
-
-        #region Overriddes Methods
-
-        protected override void WndProc(ref Message m)
-        {
-            // ReSharper disable once InconsistentNaming
-            const int WM_NCCALCSIZE = 0x0083;
-            // ReSharper disable once InconsistentNaming
-            const int WM_NCHITTEST = 0x0084;
-            const int resizeAreaSize = 10;
-
-            // ReSharper disable once InconsistentNaming
-            const int HTCLIENT = 1;
-            // ReSharper disable once InconsistentNaming
-            const int HTLEFT = 10;
-            // ReSharper disable once InconsistentNaming
-            const int HTRIGHT = 11;
-            // ReSharper disable once InconsistentNaming
-            const int HTTOP = 12;
-            // ReSharper disable once InconsistentNaming
-            const int HTTOPLEFT = 13;
-            // ReSharper disable once InconsistentNaming
-            const int HTTOPRIGHT = 14;
-            // ReSharper disable once InconsistentNaming
-            const int HTBOTTOM = 15;
-            // ReSharper disable once InconsistentNaming
-            const int HTBOTTOMLEFT = 16;
-            // ReSharper disable once InconsistentNaming
-            const int HTBOTTOMRIGHT = 17;
-
-            if (m.Msg == WM_NCHITTEST)
-            {
-                base.WndProc(ref m);
-
-                if (WindowState == FormWindowState.Normal)
-                {
-                    if ((int)m.Result == HTCLIENT)
-                    {
-                        Point screenPoint = new Point(m.LParam.ToInt32());
-                        Point clientPoint = PointToClient(screenPoint);
-
-                        if (clientPoint.Y <= resizeAreaSize)
-                        {
-                            if (clientPoint.X <= resizeAreaSize)
-                                m.Result = (IntPtr)HTTOPLEFT;
-                            else if (clientPoint.X < (Size.Width - resizeAreaSize))
-                                m.Result = (IntPtr)HTTOP;
-                            else
-                                m.Result = (IntPtr)HTTOPRIGHT;
-                        }
-                        else if (clientPoint.Y <= (Size.Height - resizeAreaSize))
-                        {
-                            if (clientPoint.X <= resizeAreaSize)
-                                m.Result = (IntPtr)HTLEFT;
-                            else if (clientPoint.X > (Width - resizeAreaSize))
-                                m.Result = (IntPtr)HTRIGHT;
-                        }
-                        else
-                        {
-                            if (clientPoint.X <= resizeAreaSize)
-                                m.Result = (IntPtr)HTBOTTOMLEFT;
-                            else if (clientPoint.X < (Size.Width - resizeAreaSize))
-                                m.Result = (IntPtr)HTBOTTOM;
-                            else
-                                m.Result = (IntPtr)HTBOTTOMRIGHT;
-                        }
-                    }
-                }
-
-                return;
-            }
-
-            // Remove border
-            if (m.Msg == WM_NCCALCSIZE && m.WParam.ToInt32() == 1)
-            {
-                return;
-            }
-
-            base.WndProc(ref m);
-        }
-
-        #endregion
-
-        #region Private Methods
-
-        private void AdjustForm()
-        {
-            switch (WindowState)
-            {
-                case FormWindowState.Maximized:
-                    Padding = new Padding(8, 8, 8, 0);
-                    break;
-
-                case FormWindowState.Normal:
-                    if (Padding.Top != BorderSize)
-                        Padding = new Padding(BorderSize);
-                    break;
-            }
-        }
-
-        #endregion
-
-        #region Form Extends Event
-
-        private void panelTitleBar_MouseDown(object sender, MouseEventArgs e)
-        {
-            ReleaseCapture();
-            SendMessage(Handle, 0x112, 0xf012, 0);
-        }
-
-        private void btnClose_Click(object sender, EventArgs e)
-        {
-            DialogResult = DialogResult.Cancel;
-            Close();
-        }
-
-        private void form_Resize(object sender, EventArgs e)
-        {
-            AdjustForm();
-
-            tbPath.Refresh();
-            tbHost.Refresh();
-            tbPass.Refresh();
-            tbUser.Refresh();
-            tbPort.Refresh();
-            tbStatus.Refresh();
-        }
-
-        private void labelTitle_MouseDown(object sender, MouseEventArgs e)
-        {
-            ReleaseCapture();
-            SendMessage(Handle, 0x112, 0xf012, 0);
-        }
-
-        #endregion
-
-        #endregion
 
         #region Private Methods
 
@@ -262,6 +93,16 @@ namespace DwUtils.Core.Forms.ConnectForms
             // Нажатие Ctrl + R
             if (e.KeyCode == Keys.R && e.Control)
                 btnCheck.PerformClick();
+        }
+
+        private void form_Resize(object sender, EventArgs e)
+        {
+            tbPath.Refresh();
+            tbHost.Refresh();
+            tbPass.Refresh();
+            tbUser.Refresh();
+            tbPort.Refresh();
+            tbStatus.Refresh();
         }
 
         #endregion
--- DwUtils/Core/Forms/EditForms/EditPlaceForm.Designer.cs
+++ DwUtils/Core/Forms/EditForms/EditPlaceForm.Designer.cs
@@ -33,101 +33,18 @@ namespace DwUtils.Core.Forms.EditForms
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EditPlaceForm));
             this.placeBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.panelTitleBar = new System.Windows.Forms.Panel();
-            this.iconPicture = new System.Windows.Forms.PictureBox();
-            this.labelTitle = new System.Windows.Forms.Label();
-            this.panelButtonTitle = new System.Windows.Forms.FlowLayoutPanel();
-            this.btnClose = new Wc32Api.Widgets.Buttons.WcButton();
             this.panelWork = new System.Windows.Forms.Panel();
             this.btnSave = new Wc32Api.Widgets.Buttons.WcButton();
             this.btnCancel = new Wc32Api.Widgets.Buttons.WcButton();
             this.cbPlace = new System.Windows.Forms.ComboBox();
             this.lblInfo = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.placeBindingSource)).BeginInit();
-            this.panelTitleBar.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.iconPicture)).BeginInit();
-            this.panelButtonTitle.SuspendLayout();
             this.panelWork.SuspendLayout();
             this.SuspendLayout();
             // 
             // placeBindingSource
             // 
             this.placeBindingSource.DataSource = typeof(DwUtils.Core.Database.Models.Place);
-            // 
-            // panelTitleBar
-            // 
-            this.panelTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
-            this.panelTitleBar.Controls.Add(this.iconPicture);
-            this.panelTitleBar.Controls.Add(this.labelTitle);
-            this.panelTitleBar.Controls.Add(this.panelButtonTitle);
-            this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelTitleBar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(56)))), ((int)(((byte)(58)))));
-            this.panelTitleBar.Location = new System.Drawing.Point(0, 0);
-            this.panelTitleBar.Name = "panelTitleBar";
-            this.panelTitleBar.Size = new System.Drawing.Size(444, 34);
-            this.panelTitleBar.TabIndex = 4;
-            this.panelTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelTitleBar_MouseDown);
-            // 
-            // iconPicture
-            // 
-            this.iconPicture.Image = global::DwUtils.Properties.Resources.web_hi_res_512;
-            this.iconPicture.Location = new System.Drawing.Point(6, 2);
-            this.iconPicture.Name = "iconPicture";
-            this.iconPicture.Size = new System.Drawing.Size(28, 28);
-            this.iconPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.iconPicture.TabIndex = 3;
-            this.iconPicture.TabStop = false;
-            // 
-            // labelTitle
-            // 
-            this.labelTitle.AutoSize = true;
-            this.labelTitle.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelTitle.ForeColor = System.Drawing.Color.Gainsboro;
-            this.labelTitle.Location = new System.Drawing.Point(43, 7);
-            this.labelTitle.Margin = new System.Windows.Forms.Padding(3, 8, 3, 0);
-            this.labelTitle.Name = "labelTitle";
-            this.labelTitle.Size = new System.Drawing.Size(208, 18);
-            this.labelTitle.TabIndex = 0;
-            this.labelTitle.Text = "DwUtils: Edit Place Form";
-            this.labelTitle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.labelTitle_MouseDown);
-            // 
-            // panelButtonTitle
-            // 
-            this.panelButtonTitle.Controls.Add(this.btnClose);
-            this.panelButtonTitle.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panelButtonTitle.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
-            this.panelButtonTitle.Location = new System.Drawing.Point(351, 0);
-            this.panelButtonTitle.Name = "panelButtonTitle";
-            this.panelButtonTitle.Padding = new System.Windows.Forms.Padding(0, 0, 3, 0);
-            this.panelButtonTitle.Size = new System.Drawing.Size(93, 34);
-            this.panelButtonTitle.TabIndex = 0;
-            // 
-            // btnClose
-            // 
-            this.btnClose.BackColor = System.Drawing.Color.Firebrick;
-            this.btnClose.BorderColor = System.Drawing.Color.Silver;
-            this.btnClose.BorderRadius = 3F;
-            this.btnClose.BorderSize = 0;
-            this.btnClose.DisableBackColor = System.Drawing.Color.DimGray;
-            this.btnClose.DisableBorderColor = System.Drawing.Color.Silver;
-            this.btnClose.FlatAppearance.BorderSize = 0;
-            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(74)))), ((int)(((byte)(84)))));
-            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnClose.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnClose.ForeColor = System.Drawing.Color.White;
-            this.btnClose.Image = global::DwUtils.Properties.Resources.x_mark_16;
-            this.btnClose.Location = new System.Drawing.Point(62, 1);
-            this.btnClose.Margin = new System.Windows.Forms.Padding(1, 1, 0, 2);
-            this.btnClose.MouseDownBackColor = System.Drawing.Color.Empty;
-            this.btnClose.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(74)))), ((int)(((byte)(84)))));
-            this.btnClose.Name = "btnClose";
-            this.btnClose.Padding = new System.Windows.Forms.Padding(2, 0, 3, 0);
-            this.btnClose.Size = new System.Drawing.Size(28, 28);
-            this.btnClose.TabIndex = 0;
-            this.btnClose.TabStop = false;
-            this.btnClose.TextColor = System.Drawing.Color.White;
-            this.btnClose.UseVisualStyleBackColor = false;
-            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // panelWork
             // 
@@ -137,9 +54,9 @@ namespace DwUtils.Core.Forms.EditForms
             this.panelWork.Controls.Add(this.cbPlace);
             this.panelWork.Controls.Add(this.lblInfo);
             this.panelWork.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelWork.Location = new System.Drawing.Point(0, 34);
+            this.panelWork.Location = new System.Drawing.Point(0, 0);
             this.panelWork.Name = "panelWork";
-            this.panelWork.Size = new System.Drawing.Size(444, 207);
+            this.panelWork.Size = new System.Drawing.Size(444, 241);
             this.panelWork.TabIndex = 5;
             // 
             // btnSave
@@ -159,7 +76,7 @@ namespace DwUtils.Core.Forms.EditForms
             this.btnSave.ForeColor = System.Drawing.Color.White;
             this.btnSave.Image = global::DwUtils.Properties.Resources.save_24;
             this.btnSave.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnSave.Location = new System.Drawing.Point(300, 149);
+            this.btnSave.Location = new System.Drawing.Point(300, 183);
             this.btnSave.MouseDownBackColor = System.Drawing.Color.Empty;
             this.btnSave.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(56)))), ((int)(((byte)(58)))));
             this.btnSave.Name = "btnSave";
@@ -189,7 +106,7 @@ namespace DwUtils.Core.Forms.EditForms
             this.btnCancel.ForeColor = System.Drawing.Color.White;
             this.btnCancel.Image = global::DwUtils.Properties.Resources.close_window_24;
             this.btnCancel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnCancel.Location = new System.Drawing.Point(30, 149);
+            this.btnCancel.Location = new System.Drawing.Point(30, 183);
             this.btnCancel.MouseDownBackColor = System.Drawing.Color.Empty;
             this.btnCancel.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(56)))), ((int)(((byte)(58)))));
             this.btnCancel.Name = "btnCancel";
@@ -238,7 +155,6 @@ namespace DwUtils.Core.Forms.EditForms
             this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(444, 241);
             this.Controls.Add(this.panelWork);
-            this.Controls.Add(this.panelTitleBar);
             this.Font = new System.Drawing.Font("Segoe UI", 10.86792F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(56)))), ((int)(((byte)(58)))));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -248,12 +164,7 @@ namespace DwUtils.Core.Forms.EditForms
             this.Text = "EditPlaceForm";
             this.Load += new System.EventHandler(this.form_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.form_KeyDown);
-            this.Resize += new System.EventHandler(this.form_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.placeBindingSource)).EndInit();
-            this.panelTitleBar.ResumeLayout(false);
-            this.panelTitleBar.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.iconPicture)).EndInit();
-            this.panelButtonTitle.ResumeLayout(false);
             this.panelWork.ResumeLayout(false);
             this.panelWork.PerformLayout();
             this.ResumeLayout(false);
@@ -262,11 +173,6 @@ namespace DwUtils.Core.Forms.EditForms
 
         #endregion
         private System.Windows.Forms.BindingSource placeBindingSource;
-        private System.Windows.Forms.Panel panelTitleBar;
-        private System.Windows.Forms.PictureBox iconPicture;
-        private System.Windows.Forms.Label labelTitle;
-        private System.Windows.Forms.FlowLayoutPanel panelButtonTitle;
-        private WcButton btnClose;
         private System.Windows.Forms.Panel panelWork;
         private System.Windows.Forms.Label lblInfo;
         private System.Windows.Forms.ComboBox cbPlace;
