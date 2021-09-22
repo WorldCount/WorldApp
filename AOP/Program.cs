373d5b868480cf20a964b5e6ed8bd6ac837057e1
WorldCount <World.Count@yandex.ru>
2021-09-22 20:41:25 +0300

    Инициализация приложения для АОП

---
+++ AOP/Program.cs
@@ -0,0 +1,27 @@
+﻿using AOP.Core.Forms;
+using NLog;
+using System;
+using System.Windows.Forms;
+
+namespace AOP
+{
+    static class Program
+    {
+
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.ThreadException += Application_ThreadException;
+            Application.Run(new GeneralForm());
+        }
+
+        private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            Logger.Error($"{e.Exception.Source}: {e.Exception}");
+        }
+    }
+}
---
+++ AOP/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("AOP")]
+[assembly: AssemblyDescription("Программа для работы с АОП")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("WorldApp")]
+[assembly: AssemblyProduct("AOP")]
+[assembly: AssemblyCopyright("Copyright WorldCount©  2021")]
+[assembly: AssemblyTrademark("WorldApp")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("1939e519-34cf-4507-8a79-85996a46bb98")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
